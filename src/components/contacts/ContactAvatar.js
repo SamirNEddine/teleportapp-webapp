7226a5ac41f751556bb6a003e6e34d6ca184e453
Samir <samir@nevereatalone.io>
2019-08-25 20:23:08 +0200

    add a basic animation to open a conversation

--- src/components/contacts/ContactAvatar.js
+++ src/components/contacts/ContactAvatar.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom'
 import './contacts.css';
 
 const ContactAvatar = function ({positionClassName, user, history}) {
+    const [openConversationAnimation, setOpenConversationAnimation] = useState(false);
     const onClick = _ => {
-        history.push({
-            pathname: '/conversation',
-            state: {firstUser: user}
-        });
+        setOpenConversationAnimation(true);
+        setTimeout(_ => {
+            history.push({
+                pathname: '/conversation',
+                state: {firstUser: user}
+            });
+        },350);
     };
     return (
-        <div className={'contact-avatar ' + positionClassName} onClick={onClick}>
+        <div className={'contact-avatar ' + positionClassName + (openConversationAnimation ? ' selected' : '')} onClick={onClick}>
             <img src={user.profilePicture} alt="avatar"/>
         </div>
     )
