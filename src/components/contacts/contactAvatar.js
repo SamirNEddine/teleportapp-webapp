91b04081cf350f7be90d23da8c925b9bca1f28cd
Samir <samir@nevereatalone.io>
2019-08-25 15:24:32 +0200

    add image css

--- src/components/contacts/contactAvatar.js
+++ src/components/contacts/contactAvatar.js
@@ -2,5 +2,9 @@ import React from 'react';
 import './contacts.css';
 
 export const ContactAvatar = function ({positionClassName}) {
-    return <div className={'contact-avatar ' + positionClassName}></div>
+    return (
+        <div className={'contact-avatar ' + positionClassName}>
+            <img src="https://storage.googleapis.com/test-teleport/joy.jpeg" alt="test"/>
+        </div>
+    )
 };
