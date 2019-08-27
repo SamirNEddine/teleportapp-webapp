e30a300db61d35f1c9705d1201373ecaf833c0c5
Samir <samir@nevereatalone.io>
2019-08-27 15:48:44 +0200

    imporve stream and client init for agora

--- src/contexts/AgoraContext.js
+++ src/contexts/AgoraContext.js
@@ -1,47 +1,47 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import AgoraRTC from "agora-rtc-sdk";
 import {AuthenticationContext} from "./AuthenticationContext";
-
-let agoraLocalStream = null;
 
 export const agoraClient = AgoraRTC.createClient({ mode: "live", codec: "h264" });
 export const AgoraContext = createContext();
 
 export const AgoraContextProvider = function({children}) {
     const {user} = useContext(AuthenticationContext);
-    if(user && !agoraLocalStream){
-        agoraLocalStream = AgoraRTC.createStream({streamID: user.userId, audio: true, video: false, screen: false});
-    }
-    const [localStream] = useState(user ? agoraLocalStream : null);
+    const [localStream, setLocalStream] = useState(null);
     const [remoteStreams, setRemoteStreams] = useState( []);
     const [agoraError, setAgoraError] = useState(null);
     const [localStreamInitialized, setLocalStreamInitialized] = useState(false);
     const [clientInitialized, setClientInitialized] = useState(false);
     const [listenersAdded, setListenersAdded] = useState(false);
-    if (!agoraError && localStream && !localStreamInitialized) {
-        //Init the local stream
-        localStream.init( _ => {
-            //Request access to microphone
-            console.log("getUserMedia successfully");
-            setLocalStreamInitialized(true);
-            setAgoraError(null);
-        }, function (err) {
-            console.log("getUserMedia failed", err);
-            setAgoraError(err);
-        });
-    }
 
-    //Init the client
-    if (!agoraError && !clientInitialized) {
-        agoraClient.init(process.env.REACT_APP_AGORA_APP_ID, function () {
-            console.log("AgoraRTC client initialized");
-            setClientInitialized(true);
-            setAgoraError(null);
-        }, function (err) {
-            console.log("AgoraRTC client init failed", err);
-            setAgoraError(err);
-        });
-    }
+    useEffect(_ => {
+        if (!localStream && user){
+            setLocalStream(AgoraRTC.createStream({streamID: user.userId, audio: true, video: false, screen: false}));
+        }
+        if(localStream && !localStreamInitialized){
+            localStream.init( _ => {
+                //Request access to microphone
+                console.log("getUserMedia successfully");
+                setLocalStreamInitialized(true);
+                setAgoraError(null);
+            }, function (err) {
+                console.log("getUserMedia failed", err);
+                setAgoraError(err);
+            });
+        }
+
+        if(!clientInitialized){
+            agoraClient.init(process.env.REACT_APP_AGORA_APP_ID, function () {
+                console.log("AgoraRTC client initialized");
+                setClientInitialized(true);
+                setAgoraError(null);
+            }, function (err) {
+                console.log("AgoraRTC client init failed", err);
+                setAgoraError(err);
+            });
+        }
+    });
+
     return (
         <AgoraContext.Provider value={{localStream, remoteStreams, setRemoteStreams, agoraError, setAgoraError, listenersAdded, setListenersAdded}}>
             {children}
