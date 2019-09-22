f46d2a4babaa1b5f50fe7dca7fcb5280cb2876b8
Samir <samir@nevereatalone.io>
2019-09-22 11:51:39 +0200

    always render the components to give it the chance the clean up before logging out

--- src/helpers/PrivateRoute.js
+++ src/helpers/PrivateRoute.js
@@ -5,11 +5,12 @@ import { AuthenticationContext } from "../contexts/AuthenticationContext";
 class PrivateRoute extends Route{
     render() {
         const { authState } = this.context;
-        if(!authState.user){
-            return <Redirect to="/signin" />
-        }else{
-            return super.render()
-        }
+        return (
+            <div>
+                {super.render()}
+                {!authState.user ?  <Redirect to="/signin" /> : ''}
+            </div>
+        )
     }
 }
 PrivateRoute.contextType = AuthenticationContext;
