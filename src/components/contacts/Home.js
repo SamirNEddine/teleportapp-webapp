e988014843a23e8e7ece17f77a4104359c953944
Samir <samir@nevereatalone.io>
2019-09-25 21:42:32 +0200

    add missing dep

--- src/components/contacts/Home.js
+++ src/components/contacts/Home.js
@@ -25,7 +25,7 @@ const Home = function ({contacts, displayInformationalText}) {
         if(conversation.isCreator && conversation.aborted){
             displayInformationalText('Failed to connect. Please try again.', 'negative');
         }
-    }, [conversation.aborted, displayInformationalText]);
+    }, [conversation.isCreator, conversation.aborted, displayInformationalText]);
     const onContactClick = async contact => {
         const channel = await generateNewConversationChannel();
         dispatch(startConversation(channel));
