51a6d9ab512d76e5b76ad1a9cc584f93c6a8b9f1
Samir <samir@nevereatalone.io>
2019-09-25 20:59:47 +0200

    revert openToken microphone implmentation

--- src/contexts/ConversationContext.js
+++ src/contexts/ConversationContext.js
@@ -18,7 +18,7 @@ import {
     contactFetched,
     conversationReducer, joinConversation,
     remoteStreamReceived,
-    remoteStreamRemoved, unmuteAudio, abortConversationAfterTimeout, updateMicrophoneAccess
+    remoteStreamRemoved, unmuteAudio, abortConversationAfterTimeout
 } from "../reducers/conversationReducer";
 import {randomString} from "../utils/utils";
 
@@ -36,7 +36,6 @@ export const ConversationContextProvider = function ({children}) {
         contactIdToAdd:null,
         muteAudio: true,
         aborted: false,
-        microphoneAccess: null,
         analytics:[]
     });
 
@@ -102,9 +101,6 @@ export const ConversationContextProvider = function ({children}) {
         if (!openTokError && openTokEventData){
             console.debug('OpenTok event:', openTokEvent, openTokEventData);
             switch(openTokEvent){
-                case OpenTokEvents.MICROPHONE_ACCESS:
-                    dispatch(updateMicrophoneAccess(openTokEventData.access));
-                    break;
                 case OpenTokEvents.REMOTE_STREAM_RECEIVED:
                     const {receivedStream} = openTokEventData;
                     const contactId = parseInt(receivedStream.name);
@@ -156,7 +152,7 @@ export const ConversationContextProvider = function ({children}) {
                     break;
             }
         }else{
-                //Todo: Error handling strategy
+            //Todo: Error handling strategy
         }
     },[voxeetError, voxeetEvent, fetchContact, dispatch, state.isCreator]);
 
