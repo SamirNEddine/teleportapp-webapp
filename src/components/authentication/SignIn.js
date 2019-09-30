7465aae44bf07a3734da3a1b2bd847492ce7827e
Samir <samir@nevereatalone.io>
2019-09-30 15:53:31 +0200

    update assets urls

--- src/components/authentication/SignIn.js
+++ src/components/authentication/SignIn.js
@@ -32,7 +32,7 @@ const SignIn = function ({history}) {
     return (
         <div className='auth-container'>
             <div className="signin-container">
-                <img src="https://storage.googleapis.com/teleport_public/logo/white.svg" className="teleport-logo" alt="Logo"/>
+                <img src="https://storage.googleapis.com/teleport_public_assets/logo/white.svg" className="teleport-logo" alt="Logo"/>
                 <form className="signin-form" onSubmit={handleSubmit}>
                     <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder="email"  autoComplete="username"/>
                     <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="password"  autoComplete="current-password"/>
